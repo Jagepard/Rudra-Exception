6ef8f54284da4f56480f51d1794d37a3f5800ebf
Jagepard <dankorot@gmail.com>
2017-03-20 15:27:00 +0300

    Изменяет use для конфигурации

--- src/DBException.php
+++ src/DBException.php
@@ -11,10 +11,22 @@
 
 namespace Rudra;
 
+use App\Config;
 use Exception;
 
 
 class DBException extends Exception
 {
-
+    /**
+     * @param $exception
+     */
+    public function handler($exception)
+    {
+        if (DEV) {
+            !d($exception->getMessage());
+        } else {
+            Container::$app->set('router', new Router(Container::$app, Config::DEFAULT_NAMESPACE));
+            Container::$app->get('router')->directCall(Config::HTTP_ERRORS['503']);
+        }
+    }
 }
