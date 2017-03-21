962b50c155f17d1b570fc5c4237189739bcb89e7
Jagepard <dankorot@gmail.com>
2017-03-21 13:50:42 +0300

    Instead of using an else block, use an early return statement:

--- src/DBException.php
+++ src/DBException.php
@@ -17,16 +17,19 @@ use Exception;
 
 class DBException extends Exception
 {
+
     /**
      * @param $exception
+     *
+     * @return bool
      */
     public function handler($exception)
     {
         if (DEV) {
-            !d($exception->getMessage());
-        } else {
-            Container::$app->set('router', new Router(Container::$app, Config::DEFAULT_NAMESPACE));
-            Container::$app->get('router')->directCall(Config::HTTP_ERRORS['503']);
+            return !d($exception->getMessage());
         }
+
+        Container::$app->set('router', new Router(Container::$app, Config::DEFAULT_NAMESPACE));
+        Container::$app->get('router')->directCall(Config::HTTP_ERRORS['503']);
     }
 }
