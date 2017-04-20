86cce3eafc82ccc6218b5155d256d982b4407c64
Jagepard <dankorot@gmail.com>
2017-04-20 14:46:21 +0300

    Добавляет AnnotationException

---
+++ src/AnnotationException.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types = 1);
+
+/**
+ * Date: 11.02.17
+ * Time: 18:16
+ *
+ * @author    : Korotkov Danila <dankorot@gmail.com>
+ * @copyright Copyright (c) 2016, Korotkov Danila
+ * @license   http://www.gnu.org/licenses/gpl.html GNU GPLv3.0
+ */
+
+namespace Rudra;
+
+
+use Exception;
+
+
+/**
+ * Class DBException
+ *
+ * @package Rudra
+ */
+class AnnotationException extends Exception
+{
+
+    /**
+     * @codeCoverageIgnore
+     * @param $exception
+     *
+     * @return bool
+     */
+    public function handler($exception)
+    {
+        if (DEV) {
+            return !d($exception->getMessage());
+        }
+
+        Container::$app->set('router', new Router(Container::$app, Config::DEFAULT_NAMESPACE));
+        Container::$app->get('router')->directCall(Config::HTTP_ERRORS['503']);
+    }
+}
