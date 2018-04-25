0bb03628a590e2dc84241fa545374427f051021f
Jagepard <dankorot@gmail.com>
2018-04-25 15:28:03 +0300

    Style updated

--- src/AnnotationException.php
+++ src/AnnotationException.php
@@ -1,6 +1,6 @@
 <?php
 
-declare(strict_types = 1);
+declare(strict_types=1);
 
 /**
  * Date: 11.02.17 Updated 25.04.18
--- src/RouterException.php
+++ src/RouterException.php
@@ -1,6 +1,6 @@
 <?php
 
-declare(strict_types = 1);
+declare(strict_types=1);
 
 /**
  * Date: 10.02.17 Updated 25.04.18
