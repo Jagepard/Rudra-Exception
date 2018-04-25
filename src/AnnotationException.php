2d841cc7045af72a73c876c85331fb40d7e91003
Jagepard <dankorot@gmail.com>
2018-04-25 14:03:03 +0300

    Реорганизовал

--- src/AnnotationException.php
+++ src/AnnotationException.php
@@ -3,7 +3,7 @@
 declare(strict_types = 1);
 
 /**
- * Date: 11.02.17
+ * Date: 11.02.17 Updated 25.04.18
  * Time: 18:16
  *
  * @author    : Korotkov Danila <dankorot@gmail.com>
@@ -13,39 +13,11 @@ declare(strict_types = 1);
 
 namespace Rudra;
 
-
-use Exception;
-
-
 /**
- * Class DBException
- *
+ * Class AnnotationException
  * @package Rudra
  */
-class AnnotationException extends Exception
+class AnnotationException extends RudraException
 {
 
-    use SetContainerTrait;
-
-    /**
-     * DBException constructor.
-     * @param ContainerInterface $container
-     * @param string             $message
-     * @param int                $code
-     * @param Exception|null     $previous
-     */
-    public function __construct(ContainerInterface $container, $message = "", $code = 0, Exception $previous = null)
-    {
-        $this->container = $container;
-        parent::__construct($message, $code, $previous);
-    }
-
-    /**
-     * @param $exception
-     */
-    public function handler($exception)
-    {
-        $this->container()->get('debugbar')['exceptions']->addException($exception);
-        $this->container()->get('router')->directCall($this->container()->config('http.errors', '503'));
-    }
 }
