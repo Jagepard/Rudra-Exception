ba207e5c84f63dcc63f3baf13e60c5c24737120c
Jagepard <dankorot@gmail.com>
2020-09-04 22:29:58 +0300

    update

--- tests/ExceptionTest.php
+++ tests/ExceptionTest.php
@@ -2,13 +2,11 @@
 
 /**
  * @author    : Jagepard <jagepard@yandex.ru">
- * @copyright Copyright (c) 2019, Jagepard
  * @license   https://mit-license.org/ MIT
  */
 
 use Rudra\Exceptions\DBException;
 use Rudra\Exceptions\RouterException;
-use Rudra\Exceptions\AnnotationException;
 use PHPUnit\Framework\TestCase as PHPUnit_Framework_TestCase;
 
 class RudraExceptionTest extends PHPUnit_Framework_TestCase
@@ -16,18 +14,12 @@ class RudraExceptionTest extends PHPUnit_Framework_TestCase
     public function testDBException()
     {
         $this->expectException(DBException::class);
-        throw new DBException('Не выбран способ работы с данными БД');
+        throw new DBException("No way to work with database data has been selected");
     }
 
     public function testRouterException()
     {
         $this->expectException(RouterException::class);
-        throw new RouterException('404');
-    }
-
-    public function testAnnotationException()
-    {
-        $this->expectException(AnnotationException::class);
-        throw new AnnotationException('Ошибка парсинга аннотаций');
+        throw new RouterException("404");
     }
 }
